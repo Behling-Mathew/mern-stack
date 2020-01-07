a02eab7f414bcd10e725608803339db5fc7e77c9
Behling-Mathew <31866125+Behling-Mathew@users.noreply.github.com>
2020-01-06 23:01:11 -0700

    .

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,21 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// Fixing Cors issue
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+    });
+    app.options("*", cors());
+
 app.use(cors());
 
 app.set('trust proxy', 1) // trust first proxy
